87a8ed0cc36a54334a86e56a6ddeb6e11907b945
aliyavandana <aliyavandana@users.noreply.github.com>
2020-06-12 17:02:28 +0530

    add localstorage for data persistance

--- src/TodoApp.tsx
+++ src/TodoApp.tsx
@@ -1,9 +1,10 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import TodoList from './TodoList'
 import './App.css';
 
 function App() {
-  const [items, setItems] = useState<IItem[]>([]);
+  const [items, setItems] = useLocalStorage("items",[]);
+
   const [text, setText] = useState('');
 
   interface IItem {
@@ -27,6 +28,9 @@ function App() {
     };
     setText('');
     setItems(items.concat(newItem))
+    // useEffect(() => {
+    //   window.localStorage.setItem('items', JSON.stringify(items));
+    // })
   }
 
   const changeItemState = (e: String) => {
@@ -35,18 +39,6 @@ function App() {
     let index = newArray.findIndex((el)=> el.text == e)
     newArray[index].isStriked = !newArray[index].isStriked
     setItems(newArray) 
-  }
-
-  const getActiveTodos = () => {
-    console.log('in get active todos')
-    let count = 0;
-    items.forEach(el => {
-      if(!el.isStriked)  {
-        count++
-      }
-      console.log(count)
-      return count;
-    })
   }
 
   return (
@@ -66,9 +58,51 @@ function App() {
         </button>
       </form>
       <TodoList items={items} changeItemState={changeItemState}/>
-      <div>Total Todos remaining: {items.filter(el=> !el.isStriked).length} out of {items.length}</div>
+      <div>Total Todos remaining: {items.filter((el : IItem)=> !el.isStriked).length} out of {items.length}</div>
     </div>
   );
+}
+
+// Hook
+function useLocalStorage(key: string, initialValue: []) {
+
+  interface IItem {
+    id: number;
+    isStriked: boolean;
+    text: string;
+  }
+
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      // Get from local storage by key
+      const item = window.localStorage.getItem(key);
+      // Parse stored json or if none return initialValue
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      // If error also return initialValue
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue = (value: IItem[])=> {
+    try {
+      // Allow value to be a function so we have same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // Save state
+      setStoredValue(valueToStore);
+      // Save to local storage
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      // A more advanced implementation would handle the error case
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
 }
 
 export default App;
